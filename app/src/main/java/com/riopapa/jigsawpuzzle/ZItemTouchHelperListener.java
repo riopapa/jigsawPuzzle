616e01b9ab0be6fa4d23c528b1027c6aedd604c9
Woncherl Ha (하원철) <riopapa@gmail.com>
2023-12-02 16:03:40 +0900

    package rename from com. to biz.

--- app/src/main/java/com/riopapa/jigsawpuzzle/ZItemTouchHelperListener.java
+++
@@ -1,8 +0,0 @@
-package com.riopapa.jigsawpuzzle;
-
-public interface ZItemTouchHelperListener {
-
-    abstract boolean onItemMove(int fromPosition, int toPosition);
-
-    void onItemSwiped(int position);
-}
