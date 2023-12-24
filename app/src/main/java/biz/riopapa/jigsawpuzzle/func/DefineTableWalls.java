a3bedaaaf8fcb52238521bd49d809b87e1338948
Woncherl Ha (하원철) <riopapa@gmail.com>
2023-12-24 14:55:17 +0900

    Issue : still piece recycler sting hanging sometimes

--- app/src/main/java/biz/riopapa/jigsawpuzzle/func/DefineTableWalls.java
+++ app/src/main/java/biz/riopapa/jigsawpuzzle/func/DefineTableWalls.java
@@ -1,5 +1,7 @@
 package biz.riopapa.jigsawpuzzle.func;
 
+import android.util.Log;
+
 import java.util.Random;
 
 import biz.riopapa.jigsawpuzzle.model.JigTable;
@@ -15,16 +17,16 @@ public class DefineTableWalls {
 
         int columns = zz.length;
         int rows = zz[0].length;
-        Random rnd = new Random(System.currentTimeMillis() & 0xffffff);
+        Random rnd = new Random(System.currentTimeMillis() & 0xfffffff);
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < columns; col++) {
                 JigTable z = new JigTable();
                 if (col == 0) {
                     z.lType = 0;
                 } else {
-                    z.lType = zz[col-1][row].rType;
+                    z.lType = zz[col - 1][row].rType;
                 }
-                if (col < columns-1) {
+                if (col < columns - 1) {
                     z.rType = 1 + rnd.nextInt(4);   // rType counts
                 } else {
                     z.rType = 0;
@@ -33,9 +35,9 @@ public class DefineTableWalls {
                 if (row == 0) {
                     z.uType = 0;
                 } else {
-                    z.uType = zz[col][row-1].dType;
+                    z.uType = zz[col][row - 1].dType;
                 }
-                if (row < rows-1)
+                if (row < rows - 1)
                     z.dType = 1 + rnd.nextInt(4);
                 else
                     z.dType = 0;
